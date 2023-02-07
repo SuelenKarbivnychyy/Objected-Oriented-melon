cf921066480945239e58615af48756e5865b91b5
suelenk <suelenmatosr@gmail.com>
2023-02-06 19:59:31 -0500

    finished further study

--- melons.py
+++ melons.py
@@ -1,9 +1,14 @@
+import random
+# from datetime import datetime
+import datetime
+
 """Classes for melon orders."""
 
 
 class AbstractiveMelonOrder:
     """An abstract base class that other Melon Orders inherit from."""
 
+    
     def __init__(self, species, qty) :
 
         self.order_type = None
@@ -12,11 +17,32 @@ class AbstractiveMelonOrder:
         self.qty = qty
         self.shipped = False
 
+        if self.qty > 100:
+            raise TooManyMelonsError("No more than 100 melons!")
+     
+
+
+    def get_base_price(self):  
+        """Splurge pricing function to choosing a random integer between 5-9 as the base price"""
+        
+        base_price = random.randint(5, 9) 
+        order_inf = datetime.datetime.now()
+        order_date = order_inf.strftime('%A')
+        order_time = order_inf.strftime('%H')     
+
+        morning_rush_price = 4
+        weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        rush_hour = [8, 9, 10, 11]
+
+        if order_date in weekdays and order_time in rush_hour:
+            base_price += morning_rush_price
+
+        return base_price
 
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        base_price = self.get_base_price()        
         christmas_melon = base_price * 1.5        
 
         if self.species == "christmas melons":
@@ -31,6 +57,17 @@ class AbstractiveMelonOrder:
 
         self.shipped = True
 
+class TooManyMelonsError(ValueError):
+    """Raises an error when attempt to order more melon than allowed.""" 
+    pass 
+
+    # def raise_error(self):
+    #     """return an error if order qty ultrapass 100"""
+
+    #     if self.qty > 100:
+    #         raise "No more than 100 melons!"
+
+
 
 class  GovernmentMelonOrder(AbstractiveMelonOrder):
     """A Government Order"""      
@@ -39,7 +76,6 @@ class  GovernmentMelonOrder(AbstractiveMelonOrder):
         super().__init__(species, qty)
 
         self.passed_inspection = False
-
 
 
     def mark_inspection(self, passed):
